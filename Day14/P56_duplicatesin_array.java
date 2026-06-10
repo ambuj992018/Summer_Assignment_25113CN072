cbaed76d6df45cc387d25abab88111274c320093
ambuj992018 <ambujkumar119419@gmail.com>
2026-06-10 23:10:58 +0530

    Update P56_duplicatesin_array.java

--- Day14/P56_duplicatesin_array.java
+++ Day14/P56_duplicatesin_array.java
@@ -14,18 +14,19 @@ public class P56_duplicatesin_array {
 
         int[] a = new int[n];
         System.out.print("Enter the elements of array : ");
-        int i, m;
+        int k;
         // LOOP TO ENTER ELEMENTS OF AN ARRAY
-        for (i = 0; i < n; i++) {
-            System.out.print("a[" + i + "] = ");
-            a[i] = num.nextInt();
+        for (k = 0; k < n; k++) {
+            System.out.print("a[" + k + "] = ");
+            a[k] = num.nextInt();
         }
         System.out.println("DUPLICATE ELEMENTS ARE : ");
-        for (i = 0; i < n; i++) {
-            for (m = i + 1; m < n; m++) {
-                if (a[i] == a[m])
+        for (int i = 0; i < n; i++) {
+            for (int m = i + 1; m < n; m++) {
+                if (a[i] == a[m]) {
                     System.out.print(a[i] + "\t");
-                break;
+                    break;
+                }
             }
         }
 
